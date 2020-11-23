1e4ce1ef50b689870dac179cc46365d173bec254
gorenkovdv <dmitrygorenkov95@gmail.com>
2020-11-23 17:16:07 +0700

    23.11

--- create_request.php
+++ create_request.php
@@ -19,18 +19,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST"):
 		SELECT '".$uid."', '".$courseID."', `GUID`, `Name`, '".$currentDatetime."', '".$currentDatetime."' FROM `courses` WHERE `ID` = '".$courseID."'";
 		$data["sql"][] = $sql;
 		if($link->query($sql)):
-			$inserted_id = $link->insert_id;
-			$sql = "INSERT INTO `requests_listeners` (`RequestID`, `CourseID`, `UserID`) VALUES ('".$inserted_id."', '".$courseID."', '".$uid."')";
+			$requestID = $link->insert_id;
+			$sql = "INSERT INTO `requests_listeners` (`RequestID`, `CourseID`, `UserID`) VALUES ('".$requestID."', '".$courseID."', '".$uid."')";
 			$data["sql"][] = $sql;
 			if($link->query($sql)):
-				$rowID = $link->insert_id;
 				$sql = "SELECT CONCAT_WS(' ', `lastname`, `firstname`, `middlename`) `fullname`, `username`, `last_update` `lastUpdate` FROM `users` WHERE `id` = '".$uid."'";
 				$data["sql"][] = array();
 				if($dbResult = $link->query($sql)):
 					while($arUser = $dbResult->fetch_assoc()):
 						$users[] = array(
 							"id" => $uid,
-							"rowID" => $rowID,
+							"requestID" => $requestID,
 							"username" => $arUser["username"],
 							"lastUpdate" => formatDate("d.m.Y H:i:s", $arResult["lastUpdate"]),
 							"fullname" => $arUser["fullname"],
